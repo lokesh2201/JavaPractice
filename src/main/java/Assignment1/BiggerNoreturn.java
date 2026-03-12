16cb9ddeb44d615d434fe4eb3ff6551dfc89f591
lokesh2201 <135096481+lokesh2201@users.noreply.github.com>
2026-03-12 22:30:45 +0530

    Second Commit

--- src/main/java/Assignment1/BiggerNoreturn.java
+++ src/main/java/Assignment1/BiggerNoreturn.java
@@ -1,10 +1,21 @@
 package Assignment1;
 
+import java.util.Scanner;
+
 public class BiggerNoreturn {
 
     public static void main(String[] args){
 
+        Scanner S = new Scanner(System.in);
 
+            int N = ReadNumber(S);
+            System.out.println(N);
+        }
+
+        public static int ReadNumber(Scanner S){
+            System.out.println("Enter an Nummber: ");
+            return S.nextInt();
+        }
 
     }
 }
